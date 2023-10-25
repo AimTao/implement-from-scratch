8ca1211e13d50e1de3bf2e429662be7f444d4dcf
AimTao <net@aimtao.net>
2023-10-25 15:28:29 +0800

    Add test of consistent_hash

--- distributed_cache/version_4_consistent_hashing/geecache/consistenthash/consistent_hash.go
+++ distributed_cache/version_4_consistent_hashing/geecache/consistenthash/consistent_hash.go
@@ -6,6 +6,13 @@ import (
 	"strconv"
 )
 
+/**
+一致性哈希解决什么问题？
+	每个数据，都会准确的分配到一个节点上。访问时，根据该数据的hash值，确定该访问哪个节点。
+	数据倾斜问题：服务器节点过少时，会导致数据无法均匀分配在各节点上。使用虚拟节点解决。
+	删除节点或增加节点时，只需要调整该节点的数据。
+*/
+
 type Hash func(data []byte) uint32 // hash 函数类型
 
 type Map struct { // 一致性哈希算法的主要数据结构
@@ -27,7 +34,8 @@ func New(replicas int, fn Hash) *Map {
 	return m
 }
 
-// Add 添加真实节点
+// Add 在哈希环 Map.keys 中加入真实节点
+// 节点名和虚拟节点名经过 hash 计算后得到哈希值，将哈希值加入 Map.keys 中，并排序。
 func (m *Map) Add(keys ...string) {
 	for _, key := range keys { // 对于每个真实节点，添加 m.replicas 个虚拟节点。
 		for i := 0; i < m.replicas; i++ {
@@ -39,7 +47,9 @@ func (m *Map) Add(keys ...string) {
 	sort.Ints(m.keys) // 对虚拟节点排序
 }
 
-// Get 选择节点
+// Get 获取 key 所在节点名
+// key 经过 hash 计算后得到哈希值，在哈希环 Map.keys 上查找最接近的节点。
+// 例如：key 的哈希值是 10000，哈希环上找到最接近的两个节点是 8000、11000，应该存在 8000 这个节点上。
 func (m *Map) Get(key string) string {
 	if len(m.keys) == 0 {
 		return ""
